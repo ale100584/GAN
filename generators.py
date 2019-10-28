b279f1cd08d040aaac96024cf141b951b8c3fa3a
Alessandro Delfino <alessandrodelfino@alessandros-mbp.home>
2019-10-28 11:55:35 +0000

    adding convolutional generator for mnist

--- generators.py
+++ generators.py
@@ -24,4 +24,37 @@ def mnist_generator(noise_shape, output_shape):
     generator.compile(loss='binary_crossentropy', optimizer='adam')
     return generator
 
+def mnist_conv_generator(noise_shape, output_shape):
+    model = models.Sequential()
+    model.add(layers.Dense(28*28*16, input_shape=noise_shape)) 
+    # It shouldn't be necessary to specify input_shape here but if I don't it sometimes generates an error. 
+    # It seems to be an issue with some versions of tensorflow https://github.com/tensorflow/tensorflow/issues/30892.
+    model.add(layers.Reshape((28,28,16)))
+    
+    model.add(layers.Conv2DTranspose(filters=64, kernel_size=[3,3], strides=(1, 1), padding='same'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    
+    model.add(layers.Conv2DTranspose(filters=32, kernel_size=[3,3], strides=(1, 1), padding='same'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    
+    model.add(layers.Conv2DTranspose(filters=16, kernel_size=[3,3], strides=(1, 1), padding='same'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    
+    model.add(layers.Conv2DTranspose(filters=1, kernel_size=[3,3], strides=(1, 1), padding='same'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
 
+    # This layer forces the output shape to be output_shape
+    model.add(layers.Flatten())
+    model.add(layers.Dense(np.prod(output_shape), activation='sigmoid'))
+    model.add(layers.Reshape(output_shape))
+    
+    noise = layers.Input(shape=noise_shape)
+    img = model(noise)
+
+    generator = models.Model(noise, img)
+    generator.compile(loss='binary_crossentropy', optimizer='adam')
+    return generator
